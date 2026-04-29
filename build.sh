2a928e74b45b36665c2dd6f2fc3cd7ce801962a4
Emmanuel-Salamba <azariahsalamba@gmail.com>
2026-04-29 13:40:19 +0000

    trying to create an auto superuser on the hosted system

--- build.sh
+++ build.sh
@@ -8,6 +8,14 @@ pip install -r requirements.txt
 python manage.py collectstatic --noinput
 
 # Apply database migrations
-python manage.py migrate
+python manage.py migrate --noinput
+
+# Create superuser if environment variables are set
+if [[ $CREATE_SUPERUSER ]];
+then
+    echo "Creating superuser..."
+    python manage.py createsuperuser --no-input --username $DJANGO_SUPERUSER_USERNAME --email $DJANGO_SUPERUSER_EMAIL
+    echo "Superuser created successfully!"
+fi
 
 echo "=== Build Complete ==="
